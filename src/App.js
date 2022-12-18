333139c1d3a7b9e8394226d6fbc17a22856d6b21
Pushkar <bansalpushkar100@gmail.com>
2022-12-18 22:34:20 +0530

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import Navbar from "./components/Navbar";
+import './static/css/app.css'
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar/>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import MenuIcon from '@mui/icons-material/Menu';
+import logo from '../static/img/logo.svg'
+// services delivery
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import AddLocationAltIcon from '@mui/icons-material/AddLocationAlt';
+import PersonIcon from '@mui/icons-material/Person';
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <div className="left-nav">
+            <MenuIcon className='menu-icon'/>
+            <img src={logo} alt="" />
+        </div>
+        <div className="right-nav">
+            <div className="service">
+            <FormControl>
+                <RadioGroup
+                    row
+                    aria-labelledby="demo-row-radio-buttons-group-label"
+                    name="row-radio-buttons-group"
+                >
+                    <FormControlLabel value="delivery" control={<Radio />} label="Delivery" />
+                    <FormControlLabel value="pickup" control={<Radio />} label="Pick Up/Dine-In" />
+                </RadioGroup>
+            </FormControl>
+            </div>
+            <div className="address">
+                <AddLocationAltIcon/>
+                <span className='text'>Select Your Location</span>
+            </div>
+            <div className="account">
+                <PersonIcon/>
+                <span className="text">My Account</span>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
